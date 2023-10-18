5834289995767cb40b06f00c4ceb9d254a382874
zekunjin <zekun.jin@foxmail.com>
2023-10-18 15:58:29 +0800

    chore: rm provider root node

--- src/hero-provider.tsx
+++ src/hero-provider.tsx
@@ -1,21 +1,15 @@
-import { defineComponent, ref, type PropType } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useProvideHeroContext, type HeroContext } from './use-hero-context'
 
-const props = {
-  as: { type: String as PropType<'div'>, default: 'div' }
-}
-
 const HeroProvider = defineComponent({
-  props,
-
-  setup (props, { slots }) {
+  setup (_, { slots }) {
     const context: HeroContext = ref({})
     useProvideHeroContext(context)
 
     return () => (
-      <props.as>
+      <>
         {slots.default?.()}
-      </props.as>
+      </>
     )
   }
 })
