6f756eda34b121bf4fe1d4c5e6283c0f76285264
Tamago <zekun.jin@foxmail.com>
2024-06-04 15:03:45 +0800

    feat: use zed

--- playground/vite/src/components/Tabs/Tab.vue
+++ playground/vite/src/components/Tabs/Tab.vue
@@ -9,8 +9,13 @@ const emit = defineEmits(['click'])
 </script>
 
 <template>
-  <button class="z-0 w-full px-3 py-1 flex group relative justify-center items-center cursor-pointer transition-opacity tap-highlight-transparent data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-30 data-[hover-unselected=true]:opacity-disabled outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 h-8 text-small rounded-small" @click="emit('click')">
-    <div class="relative z-10 whitespace-nowrap transition-colors text-black/75 text-sm group-data-[selected=true]:text-default-foreground">
+  <button
+    class="z-0 w-full px-3 py-1 flex group relative justify-center items-center cursor-pointer transition-opacity tap-highlight-transparent data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-30 data-[hover-unselected=true]:opacity-disabled outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 h-8 text-small rounded-small"
+    @click="emit('click')"
+  >
+    <div
+      class="relative z-10 whitespace-nowrap transition-colors text-black/75 text-sm"
+    >
       <slot />
     </div>
 
