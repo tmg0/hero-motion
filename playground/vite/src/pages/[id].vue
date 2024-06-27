4cd0a7499ed1e8017f02fbe328cede4c2b900f67
Tamago <zekun.jin@foxmail.com>
2024-06-27 17:58:45 +0800

    feat: use scale instead of size change

--- playground/vite/src/pages/[id].vue
+++ playground/vite/src/pages/[id].vue
@@ -11,7 +11,7 @@ const user = computed(() => data.value.find(({ username }) => username === route
 </script>
 
 <template>
-  <div class="flex flex-col gap-3">
+  <div class="flex flex-col gap-3 text-white">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 cursor-pointer" @click="router.go(-1)">
       <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
     </svg>
