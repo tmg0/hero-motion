7390f446605caca8f06304c16ea1f9a0551af072
Tamago <zekun.jin@foxmail.com>
2024-07-15 10:42:50 +0800

    feat: use HTMLElementTagNameMap

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,9 @@
 import { type ExtractPropTypes, type PropType, computed, defineComponent, ref } from 'vue'
 import { useHero } from '../composables/use-hero'
-import type { HTMLTag, Transition } from '../types'
+import type { Transition } from '../types'
 
 const props = {
-  as: { type: String as PropType<HTMLTag>, default: 'div' },
+  as: { type: String as PropType<keyof HTMLElementTagNameMap>, default: 'div' },
   layoutId: { type: [String, Number], default: undefined },
   transition: { type: Object as PropType<Partial<Transition>>, default: undefined },
   ignore: { type: Array as PropType<string[]>, default: () => [] },
