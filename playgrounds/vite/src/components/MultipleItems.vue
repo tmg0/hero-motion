e14a79188905c2fb0945d0d82b6a685ba954f3b3
Tamago <zekun.jin@foxmail.com>
2024-10-12 17:15:08 +0800

    feat: add multi items widget

--- playgrounds/vite/src/components/MultipleItems.vue
+++ playgrounds/vite/src/components/MultipleItems.vue
@@ -4,29 +4,26 @@ import { ref } from 'vue'
 
 const isSingle = ref(false)
 
-function onToggle() {
+function toggle() {
   isSingle.value = !isSingle.value
 }
 </script>
 
 <template>
   <div class="grid place-items-center h-48 w-48">
-    <button class="inline-flex justify-center rounded-md border border-transparent bg-violet-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-violet-200" @click="onToggle">
+    <button class="inline-flex justify-center rounded-md border border-transparent bg-violet-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-violet-200" @click="toggle">
       Toggle
     </button>
 
     <div class="flex gap-3">
-      <Hero v-if="isSingle" layout-id="multi-item">
-        <div class="w-24 h-24 rounded-xl cursor-pointer" :style="{ background: '#2dd4bf' }" />
-      </Hero>
+      <template v-if="isSingle">
+        <Hero layout-id="multiple-item" class="w-24 h-24 rounded-xl cursor-pointer" :style="{ background: '#f43f5e' }" />
+      </template>
 
-      <Hero v-if="!isSingle" layout-id="multi-item">
-        <div class="w-12 h-12 rounded-xl cursor-pointer" :style="{ background: '#4ade80' }" />
-      </Hero>
-
-      <Hero v-if="!isSingle" layout-id="multi-item">
-        <div class="w-12 h-12 rounded-xl cursor-pointer" :style="{ background: '#22d3ee' }" />
-      </Hero>
+      <template v-else>
+        <Hero layout-id="multiple-item" class="w-12 h-12 rounded-xl cursor-pointer" :style="{ background: '#fcd34d' }" />
+        <Hero layout-id="multiple-item" class="w-12 h-12 rounded-xl cursor-pointer" :style="{ background: '#2dd4bf' }" />
+      </template>
     </div>
   </div>
 </template>
