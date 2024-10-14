6c77f9e1333ac4181ebaa485f50015d9dc8e4083
Tamago <zekun.jin@foxmail.com>
2024-10-14 11:04:15 +0800

    feat: multi demo

--- playgrounds/vite/src/components/MultipleItems.vue
+++ playgrounds/vite/src/components/MultipleItems.vue
@@ -17,12 +17,14 @@ function toggle() {
 
     <div class="flex gap-3">
       <template v-if="isSingle">
-        <Hero layout-id="multiple-item" class="w-24 h-24 rounded-xl cursor-pointer" :style="{ background: '#f43f5e' }" />
+        <Hero layout-id="multiple-item:1" class="w-24 h-24 rounded-xl cursor-pointer float" :style="{ background: '#f43f5e' }">
+          <Hero layout-id="multiple-item:2" class="w-24 h-24 rounded-xl cursor-pointer float" :style="{ background: '#f43f5e' }" />
+        </Hero>
       </template>
 
       <template v-else>
-        <Hero layout-id="multiple-item" class="w-12 h-12 rounded-xl cursor-pointer" :style="{ background: '#fcd34d' }" />
-        <Hero layout-id="multiple-item" class="w-12 h-12 rounded-xl cursor-pointer" :style="{ background: '#2dd4bf' }" />
+        <Hero layout-id="multiple-item:1" class="w-12 h-12 rounded-xl cursor-pointer" :style="{ background: '#fcd34d' }" />
+        <Hero layout-id="multiple-item:2" class="w-12 h-12 rounded-xl cursor-pointer" :style="{ background: '#2dd4bf' }" />
       </template>
     </div>
   </div>
