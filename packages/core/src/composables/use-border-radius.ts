d76e9f45551c7b8e8ffde8529d671fe917792127
Tamago <zekun.jin@foxmail.com>
2024-11-06 11:15:34 +0800

    fix: initial border radius state

--- packages/core/src/composables/use-border-radius.ts
+++ packages/core/src/composables/use-border-radius.ts
@@ -1,37 +1,46 @@
-import { computed, type MaybeRefOrGetter, toRef } from 'vue'
+import { computed, type MaybeRef, type MaybeRefOrGetter, toRef, unref } from 'vue'
 
 interface UseScaleBorderRadiusOptions {
   scaleX: number
   scaleY: number
 }
 
-function useComputedCSSValue(value: MaybeRefOrGetter<string>, fn: (v: number) => number) {
-  const vRef = toRef(value)
+interface Position<T extends string | number = string> {
+  x: T
+  y: T
+}
+
+function useComputedPosition(value: MaybeRef<Position>, fn: (v: Position<number>) => Position<number>) {
+  const RE = /^(-?(?:\d+(?:\.\d+)?|\.\d+))([a-z%]*)$/i
 
   return computed(() => {
-    if (!vRef.value)
-      return '0'
+    const { x, y } = unref(value)
 
-    const v = [0, 'px']
-    const match = vRef.value.match(/^(-?(?:\d+(?:\.\d+)?|\.\d+))([a-z%]*)$/i)
+    const matchX = x.match(RE)
+    const matchY = y.match(RE)
 
-    if (!match)
-      return vRef.value
+    if (!matchX || !matchY)
+      return { x, y }
 
-    const [_, n, u] = match
-    v[0] = fn(Number.parseFloat(n))
-    v[1] = u || 'px'
+    const [, nx, ux] = matchX
+    const [, ny, uy] = matchY
 
-    return v.join('')
+    const p = fn({ x: Number.parseFloat(nx), y: Number.parseFloat(ny) })
+
+    return {
+      x: [p.x, ux].join(''),
+      y: [p.y, uy].join(''),
+    }
   })
 }
 
 function parseBorderRadius(value: string | number) {
-  if (!value)
-    return { x: '0', y: '0' }
+  const defaults = { x: String(value), y: String(value) }
 
+  if (!value)
+    return defaults
   if (typeof value === 'number')
-    return { x: String(value), y: String(value) }
+    return defaults
 
   const [x, y] = value.split(' ')
 
@@ -41,42 +50,35 @@ function parseBorderRadius(value: string | number) {
   }
 }
 
-export function useBorderRadius(value: MaybeRefOrGetter<Record<string, string>>) {
-  const style = toRef(value)
-  const bl = computed(() => parseBorderRadius(style.value.borderBottomLeftRadius))
-  const br = computed(() => parseBorderRadius(style.value.borderBottomRightRadius))
-  const tl = computed(() => parseBorderRadius(style.value.borderTopLeftRadius))
-  const tr = computed(() => parseBorderRadius(style.value.borderTopRightRadius))
-
-  return {
-    bl,
-    br,
-    tl,
-    tr,
-  }
-}
-
 export function useScaleBorderRadius(value: MaybeRefOrGetter<Record<string, string | number>>, options: MaybeRefOrGetter<Partial<UseScaleBorderRadiusOptions>> = {}) {
   const props = toRef(options)
   const style = toRef(value)
-  const bl = computed(() => parseBorderRadius(style.value.borderBottomLeftRadius))
-  const br = computed(() => parseBorderRadius(style.value.borderBottomRightRadius))
-  const tl = computed(() => parseBorderRadius(style.value.borderTopLeftRadius))
-  const tr = computed(() => parseBorderRadius(style.value.borderTopRightRadius))
 
-  const blX = useComputedCSSValue(bl.value.x, v => v * (props.value?.scaleX ?? 1))
-  const blY = useComputedCSSValue(bl.value.y, v => v * (props.value?.scaleY ?? 1))
-  const brX = useComputedCSSValue(br.value.x, v => v * (props.value?.scaleX ?? 1))
-  const brY = useComputedCSSValue(br.value.y, v => v * (props.value?.scaleY ?? 1))
-  const tlX = useComputedCSSValue(tl.value.x, v => v * (props.value?.scaleX ?? 1))
-  const tlY = useComputedCSSValue(tl.value.y, v => v * (props.value?.scaleY ?? 1))
-  const trX = useComputedCSSValue(tr.value.x, v => v * (props.value?.scaleX ?? 1))
-  const trY = useComputedCSSValue(tr.value.y, v => v * (props.value?.scaleY ?? 1))
+  const oBl = computed(() => parseBorderRadius(style.value.borderBottomLeftRadius))
+  const oBr = computed(() => parseBorderRadius(style.value.borderBottomRightRadius))
+  const oTl = computed(() => parseBorderRadius(style.value.borderTopLeftRadius))
+  const oTr = computed(() => parseBorderRadius(style.value.borderTopRightRadius))
 
-  return computed(() => ({
-    borderBottomLeftRadius: [blX.value, blY.value].join(' '),
-    borderBottomRightRadius: [brX.value, brY.value].join(' '),
-    borderTopLeftRadius: [tlX.value, tlY.value].join(' '),
-    borderTopRightRadius: [trX.value, trY.value].join(' '),
+  function scale(p: Position<number>) {
+    return {
+      x: p.x * (props.value?.scaleX ?? 1),
+      y: p.y * (props.value?.scaleY ?? 1),
+    }
+  }
+
+  const bl = useComputedPosition(oBl, scale)
+  const br = useComputedPosition(oBr, scale)
+  const tl = useComputedPosition(oTl, scale)
+  const tr = useComputedPosition(oTr, scale)
+
+  const borderRadius = computed(() => ({
+    borderBottomLeftRadius: [bl.value.x, bl.value.y].join(' '),
+    borderBottomRightRadius: [br.value.x, br.value.y].join(' '),
+    borderTopLeftRadius: [tl.value.x, tl.value.y].join(' '),
+    borderTopRightRadius: [tr.value.x, tr.value.y].join(' '),
   }))
+
+  return {
+    borderRadius,
+  }
 }
