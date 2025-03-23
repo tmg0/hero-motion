4b00ac5ebb6fd5346015f7ee77ac68cfc8c60f54
tmg0 <zekun.jin@foxmail.com>
2025-03-23 09:12:56 +0800

    chore: upgrade deps

--- playgrounds/vite/src/components/Tabs/Tabs.vue
+++ playgrounds/vite/src/components/Tabs/Tabs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="inline-flex">
-    <div class="flex p-1 h-fit gap-2 items-center flex-nowrap overflow-x-scroll scrollbar-hide bg-gray-100 rounded-xl">
+    <div class="flex p-1 h-fit gap-2 items-center flex-nowrap overflow-x-hidden bg-gray-100 rounded-xl">
       <slot />
     </div>
   </div>
